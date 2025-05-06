25e7232b8c354259ee0a76edf6b40185c9a675fb
yraosmruthi <yraosmruthi@gmail.com>
2025-05-07 00:44:30 +0530

    added

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+          Montserrat: ['Montserrat', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  };
+  
+  
