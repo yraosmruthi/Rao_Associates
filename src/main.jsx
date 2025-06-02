3395e3d205d034e672cde40002e1876bec95c657
yraosmruthi <133221715+yraosmruthi@users.noreply.github.com>
2025-06-02 11:48:46 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router";
+import { ViteReactSSG } from 'vite-react-ssg'
 import './index.css'
-import App from './App.jsx'
+import { routes } from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-  <StrictMode>
-    <App />
-  </StrictMode>
-</BrowserRouter>
-,
+export const createRoot = ViteReactSSG(
+  // react-router-dom data routes
+  { routes },
+  // function to have custom setups
+  ({ router, routes, isClient, initialState }) => {
+    // do something.
+  },
 )
